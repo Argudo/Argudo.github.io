22046e6c50f036f9265b77a58f231d6b59c07cdc
José Luis Argudo Garrido <68852472+Argudo@users.noreply.github.com>
2022-12-10 18:28:19 +0100

    rebase

--- index.js
+++ index.js
@@ -12,42 +12,6 @@ function toMonthName(monthNumber) {
 }
 
 let dropArea = document.getElementById('drop_zone')
-dropArea.addEventListener('dragleave', handlerFunction, false)
-dropArea.addEventListener('dragover', handlerFunction, false)
-dropArea.addEventListener('drop', handlerFunction, false)
-
-;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
-    dropArea.addEventListener(eventName, preventDefaults, false)
-})
-
-function preventDefaults (e) {
-    e.preventDefault()
-    e.stopPropagation()
-}
-;['dragenter', 'dragover'].forEach(eventName => {
-    dropArea.addEventListener(eventName, highlight, false)
-})
-
-;['dragleave', 'drop'].forEach(eventName => {
-    dropArea.addEventListener(eventName, unhighlight, false)
-})
-
-function highlight(e) {
-    dropArea.classList.add('highlight')
-}
-
-function unhighlight(e) {
-    dropArea.classList.remove('highlight')
-}
-
-dropArea.addEventListener('drop', handleDrop, false)
-
-function handleDrop(e) {
-    let dt = e.dataTransfer
-    let files = dt.files
-
-    handleFiles(files)
-}
 
 function handleFiles(files) {
     ([...files]).forEach(f => CalcularEstadisticas(f))
