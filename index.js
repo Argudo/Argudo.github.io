9b2ee2251801a4570012071f7bfdf18d002d5afd
José Luis Argudo Garrido <68852472+Argudo@users.noreply.github.com>
2022-12-10 16:43:03 +0100

    dev

--- index.js
+++ index.js
@@ -14,103 +14,107 @@ function toMonthName(monthNumber) {
 let dropArea = document.getElementById('drop_zone')
 
 function handleFiles(files) {
-    window.alert("Ejecución");
-    console.log("Ejecución");
     ([...files]).forEach(f => CalcularEstadisticas(f))
 }
   
 
 
 function CalcularEstadisticas(file){
-    var aMensajes = [];
-    Usuario.aUsuarios = [];
-    var ul = document.getElementsByTagName('ul')[0];
-    if(ul != null) ul.remove();
-    var reader = new FileReader();
-    reader.readAsText(file);
-    reader.addEventListener('load', () => {
-        console.log(reader.result);
-        aMsg = reader.result.split('\n');;
-        for(msg of aMsg){
-           if(msg != null && msg.length > 20) aMensajes.push(new Mensaje(msg));
-        }
-        var chat = new Chat(aMensajes);
-        console.log(chat);
-        window.alert(chat.NumMensajes());
-        infoch.innerHTML = `<h3 id="Usuarios"><b>Conversación entre ${Usuario.aUsuarios[0].getNombre()} y ${Usuario.aUsuarios[1].getNombre()} </b></h3>
-        <div>
-            <div id="output">
-                <p>Mensajes totales: ${aMensajes.length}</p>
-                <p>Fecha con más mensajes: ${chat.FechaMasMensajes()}</p>
-                <p>Día del mes que mas habláis: ${chat.DiaMasMensajes()}</p>
-                <p>Mes del año que mas habláis: ${toMonthName(chat.MesMasMensajes())}</p>
-                <p>Año con más mensajes: ${chat.AñoMasMensajes()}</p>
-                <p>Día de la semana que mas habláis: ${(chat.DiaSemanaMasMensajes())}</p>
-                <p>Usuario con más mensajes: ${chat.UsuarioMasMensajes()}</p>
-                <p>Usuario con más palabras: ${chat.UsuarioMasPalabras()}</p>
-            </div>
-            <canvas id="msgxdia" style="background: white; width: 40%"></canvas>
-            <canvas id="msgxmes" style="background: white; width: 40%"></canvas>
-            <canvas id="msgxuser" style="background: white; width: 40%"></canvas>
-            <button class="button" id="btnConvert" onclick="GenerarImagen()">Exportar a PNG</button>
-        </div>`;
-        new Chart(document.getElementById("msgxdia"), {
-            type: 'bar',
-            data: {
-              labels: Object.keys(chat.sDiaSemanaFrecuencia()),
-              datasets: [{
-                label: 'Mensajes por día de la semana',
-                data: Object.values(chat.sDiaSemanaFrecuencia()),
-                borderWidth: 1
-              }]
-            },
-            options: {
-              scales: {
-                y: {
-                  beginAtZero: true
-                }
-              }
+    try{    
+        var aMensajes = [];
+        Usuario.aUsuarios = [];
+        var ul = document.getElementsByTagName('ul')[0];
+        if(ul != null) ul.remove();
+        window.alert("Prelectura");
+        var reader = new FileReader();
+        reader.readAsText(file);
+        reader.addEventListener('load', () => {
+            console.log(reader.result);
+            aMsg = reader.result.split('\n');;
+            for(msg of aMsg){
+               if(msg != null && msg.length > 20) aMensajes.push(new Mensaje(msg));
             }
-        });
-        new Chart(document.getElementById("msgxuser"), {
-            type: 'bar',
-            data: {
-              labels: Object.keys(chat.UsuarioFrecuencia()),
-              datasets: [{
-                label: 'Mensajes por usuario',
-                data: Object.values(chat.UsuarioFrecuencia()),
-                borderWidth: 1
-              }]
-            },
-            options: {
-              scales: {
-                y: {
-                  beginAtZero: true
+            var chat = new Chat(aMensajes);
+            console.log(chat);
+            window.alert(chat.NumMensajes());
+            infoch.innerHTML = `<h3 id="Usuarios"><b>Conversación entre ${Usuario.aUsuarios[0].getNombre()} y ${Usuario.aUsuarios[1].getNombre()} </b></h3>
+            <div>
+                <div id="output">
+                    <p>Mensajes totales: ${aMensajes.length}</p>
+                    <p>Fecha con más mensajes: ${chat.FechaMasMensajes()}</p>
+                    <p>Día del mes que mas habláis: ${chat.DiaMasMensajes()}</p>
+                    <p>Mes del año que mas habláis: ${toMonthName(chat.MesMasMensajes())}</p>
+                    <p>Año con más mensajes: ${chat.AñoMasMensajes()}</p>
+                    <p>Día de la semana que mas habláis: ${(chat.DiaSemanaMasMensajes())}</p>
+                    <p>Usuario con más mensajes: ${chat.UsuarioMasMensajes()}</p>
+                    <p>Usuario con más palabras: ${chat.UsuarioMasPalabras()}</p>
+                </div>
+                <canvas id="msgxdia" style="background: white; width: 40%"></canvas>
+                <canvas id="msgxmes" style="background: white; width: 40%"></canvas>
+                <canvas id="msgxuser" style="background: white; width: 40%"></canvas>
+                <button class="button" id="btnConvert" onclick="GenerarImagen()">Exportar a PNG</button>
+            </div>`;
+            new Chart(document.getElementById("msgxdia"), {
+                type: 'bar',
+                data: {
+                  labels: Object.keys(chat.sDiaSemanaFrecuencia()),
+                  datasets: [{
+                    label: 'Mensajes por día de la semana',
+                    data: Object.values(chat.sDiaSemanaFrecuencia()),
+                    borderWidth: 1
+                  }]
+                },
+                options: {
+                  scales: {
+                    y: {
+                      beginAtZero: true
+                    }
+                  }
                 }
-              }
-            }
-        });
-        new Chart(document.getElementById("msgxmes"), {
-            type: 'bar',
-            data: {
-              labels: Object.keys(chat.sMesFrecuencia()),
-              datasets: [{
-                label: 'Mensajes por mes',
-                data: Object.values(chat.MesFrecuencia()),
-                borderWidth: 1
-              }]
-            },
-            options: {
-              scales: {
-                y: {
-                  beginAtZero: true
+            });
+            new Chart(document.getElementById("msgxuser"), {
+                type: 'bar',
+                data: {
+                  labels: Object.keys(chat.UsuarioFrecuencia()),
+                  datasets: [{
+                    label: 'Mensajes por usuario',
+                    data: Object.values(chat.UsuarioFrecuencia()),
+                    borderWidth: 1
+                  }]
+                },
+                options: {
+                  scales: {
+                    y: {
+                      beginAtZero: true
+                    }
+                  }
                 }
-              }
-            }
+            });
+            new Chart(document.getElementById("msgxmes"), {
+                type: 'bar',
+                data: {
+                  labels: Object.keys(chat.sMesFrecuencia()),
+                  datasets: [{
+                    label: 'Mensajes por mes',
+                    data: Object.values(chat.MesFrecuencia()),
+                    borderWidth: 1
+                  }]
+                },
+                options: {
+                  scales: {
+                    y: {
+                      beginAtZero: true
+                    }
+                  }
+                }
+            });
+    
+            document.getElementById('info').style.visibility = 'visible';
         });
-
-        document.getElementById('info').style.visibility = 'visible';
-    });
+    }
+    catch(e){
+        window.alert("Error al procesar el archivo " + e);
+    }
 }
 
 function GenerarImagen(){
